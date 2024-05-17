abe6a763f24beddcbe09a057a2e3a0b15325cf2c
mighty808 <effahaboagye888@gmail.com>
2024-05-17 15:29:39 +0000

    added examples to each task

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -4,8 +4,25 @@
 function processArray(numbers) {
     return numbers.map(num => num % 2 === 0 ? num * num : num * 3);
 }
+// for instance
+const result = processArray([1, 3, 4, 6]);
+console.log(result);
+// This gives us a result of [3, 9, 16, 36].
+
+
+
+
+
 
 // Task 2: Function formatArrayStrings
 function formatArrayStrings(strings, numbers) {
-    return strings.map((str, i) => numbers[i] % 2 === 0 ? str.toUpperCase() : str.toLowerCase());
+    return strings.map((str, i) => 
+        numbers[i] % 2 === 0 ? str.toUpperCase() : str.toLowerCase());
 }
+
+// for instance
+const strings = ['Effah', 'Paakwesi', 'Dodge', 'Challenger'];
+const numbers = processArray([1, 2, 3, 4]);
+const formattedStrings = formatArrayStrings(strings, numbers); 
+console.log(formattedStrings);
+// the formatted strings gives us [ 'effah', 'PAAKWESI', 'dodge', 'CHALLENGER' ]
