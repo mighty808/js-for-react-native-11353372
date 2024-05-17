cf7907bc176f70fb4341173a6940dbdabca8cf6c
mighty808 <effahaboagye888@gmail.com>
2024-05-17 15:33:54 +0000

    added examples to task 3

--- userInfo.js
+++ userInfo.js
@@ -6,3 +6,20 @@ function createUserProfiles(names, modifiedNames) {
         id: i + 1
     }));
 }
+
+
+// Example usage:
+const originalNames = ["Effah", "Nana", "Blacko", "Izzy", "John"];
+const modifiedNames = ["EFFAH", "NANA", "BLACKO", "IZZY", "JOHN"];
+
+const userProfiles = createUserProfiles(originalNames, modifiedNames);
+console.log(userProfiles);
+/* Output:
+[
+  { originalName: 'Effah', modifiedName: 'EFFAH', id: 1 },
+  { originalName: 'Nana', modifiedName: 'NANA', id: 2 },
+  { originalName: 'Blacko', modifiedName: 'BLACKO', id: 3 },
+  { originalName: 'Izzy', modifiedName: 'IZZY', id: 4 },
+  { originalName: 'John', modifiedName: 'JOHN', id: 5 }
+]
+*/
